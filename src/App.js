0fdc8b1839b69e9b7420678b2ace3e5451105728
baguetteTHM <theohuguesmartin@gmail.com>
2022-10-01 17:55:22 +0200

    basic router setup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
-function App() {
+import { Link, Route, Routes,NavLink } from 'react-router-dom'
+import Details from './pages/Details/Details'
+import  Home  from './pages/Home/Home'
+
+
+export default function App() {
   return (
-    <div > yo
-    </div>
-  );
+    <>
+    <nav>
+      <ul>
+        <li><NavLink to="/">Home</NavLink></li>
+        <li><NavLink to="/propos">A propos</NavLink></li>
+      </ul>
+    </nav>
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path='/propos'element={<Details/>}/>
+
+    </Routes>
+    <div>Mon Footer</div>
+    </>
+  )
 }
 
-export default App;
