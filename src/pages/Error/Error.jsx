f0db4f55e9ed5a6c0862acabdf369d10d8ca3c95
baguetteTHM <theohuguesmartin@gmail.com>
2022-10-02 00:01:40 +0200

    error page speedrun

--- src/pages/Error/Error.jsx
+++ src/pages/Error/Error.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import "./Error.css"
+import { Link } from 'react-router-dom'
 
 export default function Error() {
   return (
-    <div>404</div>
+    <div className='error__container'>
+      <span className='error__number'>404</span>
+      <p className='error__message'>Oups! La page que vous demandez n'existe pas.</p>
+      <p  className='error__link'> <Link to='/'> Retourner sur la page d’accueil</Link></p>
+
+    </div>
   )
 }
