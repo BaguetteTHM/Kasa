fc45196b702fdd9e703a6dcf6562473c8696c87f
baguetteTHM <theohuguesmartin@gmail.com>
2022-10-03 05:05:09 +0200

    not sure about idvalidator

--- src/App.js
+++ src/App.js
@@ -2,10 +2,10 @@ import { Route, Routes } from 'react-router-dom'
 import Details from './pages/Details/Details'
 import  Home  from './pages/Home/Home'
 import Error from './pages/Error/Error'
-import Logement from './pages/Logement/Logement'
 import Header from './components/Header/Header'
 import Footer from './components/Footer/Footer'
 import "./App.css"
+import IdValidator from './pages/Logement/IdValidator'
 
 
 export default function App() {
@@ -16,7 +16,7 @@ export default function App() {
     <Routes>
       <Route path='/' element={<Home/>}/>
       <Route path='/propos'element={<Details/>}/>
-      <Route path='/:id'element={<Logement/>}/>
+      <Route path='/:id'element={<IdValidator/>}/>
       <Route path='*' element={<Error/>}/>
 
     </Routes>
