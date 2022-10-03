7d93015a629a8757133c0afc792a9d3e9e167e14
baguetteTHM <theohuguesmartin@gmail.com>
2022-10-03 03:03:26 +0200

    bullet arrow hiding when 1 picture fixed

--- src/components/Slideshow/Slideshow.jsx
+++ src/components/Slideshow/Slideshow.jsx
@@ -20,9 +20,12 @@ export default function Slideshow({slides}) {
 
     return (
     <div className='slideshow'>
+        {length > 1 &&
+        <>
         <span className='leftarrow' onClick={previousSlide}><svg width="48" height="80" viewBox="0 0 48 80" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M47.04 7.78312L39.92 0.703125L0.359985 40.3031L39.96 79.9031L47.04 72.8231L14.52 40.3031L47.04 7.78312Z" fill="white"/></svg></span>
         <span className='rightarrow' onClick={nextSlide}><svg width="48" height="80" viewBox="0 0 48 80" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M0.960022 72.3458L8.04002 79.4258L47.64 39.8258L8.04002 0.22583L0.960022 7.30583L33.48 39.8258L0.960022 72.3458Z" fill="white"/></svg></span>
-        <span className='bullet'>{current + 1}/{length}</span>
+        
+        <span className='bullet'>{current + 1}/{length}</span></>}
         <img className='slideshow__img' src={slides[current]} alt="current logement"/>
         
     </div>
