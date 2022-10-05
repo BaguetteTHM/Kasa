62e885bd8adbe00237a5f0fd70500eb7c65d720d
baguetteTHM <theohuguesmartin@gmail.com>
2022-10-05 23:50:16 +0200

    same

--- src/components/Dropdown/Dropdown.jsx
+++ src/components/Dropdown/Dropdown.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { useState } from 'react'
 import './Dropdown.css'
+import arrowUp from '../../assets/arrowUp.png'
+import arrowDown from '../../assets/arrowDown.png'
 
 export default function Dropdown({guideline}) {
 
@@ -13,10 +15,8 @@ export default function Dropdown({guideline}) {
     <div className='dropdown'>
       <div className='dropdown__button'>
         <span className='dropdown__button__title'>{guideline.title}</span>
-        <span className='dropdown__button__icon'onClick={drop}>{!isOpen ?<svg width="25" height="15" viewBox="0 0 25 15" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path d="M2.66344 0.85955L0.530518 3.00468L12.4604 14.9234L24.3903 2.99263L22.2574 0.85955L12.4604 10.6572L2.66344 0.85955Z" fill="white"/>
-        </svg>
-        : <svg width="24" height="15" viewBox="0 0 24 15" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M21.7268 14.1403L23.8597 11.9951L11.9298 0.0764155L-5.01125e-05 12.0072L2.13287 14.1403L11.9298 4.34257L21.7268 14.1403Z" fill="white"/></svg>
+        <span className='dropdown__button__icon'onClick={drop}>{!isOpen ?<img src={arrowDown} alt="arrow down icon to drop" />
+        : <img src={arrowUp} alt="arrow up icon to close dropdown" />
         }</span>
       </div>
       {isOpen && 
